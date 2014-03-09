21da96318f73cdd512bc92b5752ef0277eda6824
Zachary Robert Thomas Boyd <zacharyboyd@znetstar.net>
2014-03-09 00:52:06 -0500

    _

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 	define("SOCKS_PORT", "9050");
 	define("CHECK_INSTALL", "polipo,haproxy,tor,screen");
 	define("CACHE_DIR", "");
-	define("REMOTE_LOGGING_HOST", "");
+	define("REMOTE_LOGGING_HOST", "logs.papertrailapp.com:59362");
 	define("PROXY_USERNAME", "da1b91ca-b548-48ab-8c42-a556dd95d2f0");
 	define("PROXY_PASSWORD", "1cdf3c32-7699-4ed2-ad88-7e9897c8f7fc");
 	define("UPDATE_URL", "http://bit.ly/tor-router-zip");
