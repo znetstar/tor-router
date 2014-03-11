f2ad05bb02a7745428d2ab085848631a89b07cf3
Zachary Robert Thomas Boyd <zacharyboyd@znetstar.net>
2014-03-11 18:33:37 -0400

    _

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 	define("CONTROL_PORT", "15000");
 	define("TOR_DATA_DIR", "/data");
 	define("PROXY_PORT", "8123");
-	define("INSTANCES", "25");
+	define("INSTANCES", "15");
 	define("BALANCE_MODE", "roundrobin");
 	define("SOCKS_PORT", "9050");
 	define("CHECK_INSTALL", "polipo,haproxy,tor,screen");
-	define("CACHE_DIR", "");
+	define("CACHE_DIR", "/var/cache/polipo");
 	define("REMOTE_LOGGING_HOST", "logs.papertrailapp.com:59362");
 	define("PROXY_USERNAME", "da1b91ca-b548-48ab-8c42-a556dd95d2f0");
 	define("PROXY_PASSWORD", "1cdf3c32-7699-4ed2-ad88-7e9897c8f7fc");
