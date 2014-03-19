be2366659230d66ca788dc3a25e22ea7fffb9274
Zachary Boyd <zacharyboyd@znetstar.net>
2014-03-19 13:22:31 +0000

    _

--- test.sh.php
+++ test.sh.php
@@ -1,13 +1,10 @@
 <?php require('./config.php'); ?>
 #!/bin/bash
 
-while :
-do
-	if export http_proxy=http://<?php echo PROXY_USERNAME; ?>:<?php echo PROXY_PASSWORD; ?>@localhost:<?php echo PROXY_PORT; ?> && wget -qO- http://ifconfig.me/ip; then
-		sleep 1;
-	else 
-		echo "test failed";
-		init 6;
-		break;
-	fi
-done
+if export http_proxy=http://<?php echo PROXY_USERNAME; ?>:<?php echo PROXY_PASSWORD; ?>@localhost:<?php echo PROXY_PORT; ?> && wget -qO- http://ifconfig.me/ip; then
+		exit 0;
+else 
+	echo "test failed";
+	init 6;
+	exit 1;
+fi
