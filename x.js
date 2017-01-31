40edf63c5635556f2431c18ffc50874af297479a
Zachary Boyd <zachary@zacharyboyd.nyc>
2017-01-31 18:12:31 -0500

    final

--- x.js
+++
@@ -1,9 +0,0 @@
-const S = require('./').SOCKSServer;
-const P = require('./').TorPool;
-
-let p = new P('tor', null, require('winston'));
-let s = new S(p, require('winston'));
-
-p.create(3, () => {
-	s.listen(9050);
-});
