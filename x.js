ec0c99234981b355360f991cc9b5a2ff43c3e5c9
Zachary Boyd <zachary@zacharyboyd.nyc>
2017-03-22 12:45:16 -0400

    adds control server

--- x.js
+++
@@ -1,8 +0,0 @@
-const shell = require('shelljs');
-const fs = require('fs');
-const DIR = '/tmp/x';
-shell.find(DIR)
-	.filter((path) => fs.lstatSync(path).isFile())
-	.forEach((file) => {
-		
-	});
